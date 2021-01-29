d5496b80d69d9747c9d8e1f8e92f13ed21f98f77
Scott Silliman <=>
2021-01-29 17:09:33 -0500

    redefined as class

--- src/App2.js
+++ src/App2.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -8,41 +8,113 @@ import MDBModal from "./components/MDBModal"
 
 import CheckList from "./components/CheckList";
 
-const App = (props) => {
-    // const [infoPaneCollapsed, setInfoPaneCollapsed] = useState(true);
-    // const toggleInfoPane = () => setInfoPaneCollapsed(!infoPaneCollapsed);
-    const [showModal, setShowModal] = useState(false);
-    const toggleModal = () => setShowModal(!showModal);
+class App extends React.Component {
+
+ constructor(props) {
+   super(props);
+   this.state = {
+     callouts: {},
+     vitals: {},
+     showScenarioInfoModal: false,
+     currentNode: null,
+     checkListItems: {},
+     criticalCriteria: {}
+   };
+    // bind event event handlers;
+    this.toggleScenarioInfoModal = this.toggleScenarioInfoModal.bind(this)
+    this.setCurrentNode = this.setCurrentNode.bind(this)
+    this.toggleCheckListItem = this.toggleCheckListItem.bind(this)
+    // state accessor needs to be bound also as 'this' isn't defined in
+    // function components
+    this.getCheckedState = this.getCheckedState.bind(this)
+  }
+
+
+  toggleScenarioInfoModal(event) {
+    this.setState((state, props) => ({
+      showScenarioInfoModal: !state.showScenarioInfoModal
+    }))
+  }
+
+  setCurrentNode(node) {
+    console.log(node);
+    this.setState((state, props) => ({
+      currentNode: node
+    }));
+  }
+
+  getCheckedState(id) {
+    return this.state.checkListItems[id]
+  }
+
+  toggleCheckListItem(node) {
+    // console.log(node)
+
+    this.setState((prevState) => ({
+      checkListItems: { ...prevState.checkListItems, [node]: !prevState.checkListItems[node] }
+    }))
+  }
+
+  componentDidMount() {
+    let scen = this.props.scenario;
+    // console.log(scen)
+    let crits = {}
+    scen.criticalCriteria.forEach((cc) => { crits[cc.id] = false });
+    let cli = {}
+    // eslint-disable-next-line
+    for (const [key, value] of Object.entries(scen.items)) {
+      value.forEach((item) => {
+        cli[item.id] = false;
+      })
+    }
+    this.setState({
+      // ui-related state vars
+      callouts: {},
+      vitals: {},
+      showScenarioInfoModal: false,
+      currentNode: null,
+      // domain stuff (log to db)
+      checkListItems: cli,
+      criticalCriteria: crits
+    });
+  }
+
+  render() {
+    const scen = this.props.scenario;
 
     const lhs = <CheckList
+                  stateGetter={this.getCheckedState}
+                  stateToggler={this.toggleCheckListItem}
                   key="first"
                   heading="Checklist"
-                  steps={props.scenario.steps} />
-    const rhs = (<div></div>)
+                  steps={scen.steps} />
+    // const lhs = (<div>placeholder</div>)
+    const rhs = (<div>placeholder</div>)
 
-   return (
-      <>
-        <MDBNavBar
-          branding={props.branding}
-          toggler={toggleModal}
-        />
-        <MDBContainer
-          lhs={lhs}
-          rhs={rhs}
-        />
-        <MDBModal
-          title="Scenario Info"
-          show={showModal}
-          toggler={toggleModal}
-        >
-          <h3>Dispatch Information</h3>
-          <p>{props.scenario.info.dispatchInfo}</p>
-          <h3>Scene Assessment</h3>
-          <p>{props.scenario.info.sceneAssessment}</p>
-        </MDBModal>
-      </>
-    );
-
+    return (
+        <>
+          <MDBNavBar
+            branding="Checklist w/Tabs"
+            title={scen.info.name}
+            toggler={this.toggleScenarioInfoModal}
+          />
+          <MDBContainer
+            lhs={lhs}
+            rhs={rhs}
+          />
+          <MDBModal
+            title="Scenario Info"
+            show={this.state.showScenarioInfoModal}
+            toggler={this.toggleScenarioInfoModal}
+          >
+            <h3>Dispatch Information</h3>
+            <p>{scen.info.dispatchInfo}</p>
+            <h3>Scene Assessment</h3>
+            <p>{scen.info.sceneAssessment}</p>
+          </MDBModal>
+        </>
+      );
+    }
 }
 
 export default App;
