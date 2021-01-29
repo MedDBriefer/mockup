1c1ff650a2c05aba49e83a018c19800efd2e2a5c
Scott Silliman <=>
2021-01-29 17:10:52 -0500

    indented some code

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -3,10 +3,13 @@ import React from "react";
 import CallOut from "./CallOut";
 
 const CheckBox = ({ step, stateGetter, stateToggler }) => {
-  let isChecked = stateGetter(step.id)
+
   return (
       <label className={step.type === 'critical-criteria' ? "text-danger" : "text-default"}>
-        <input type="checkbox" checked={isChecked} onChange={() => stateToggler(step.id)} />
+        <input
+          type="checkbox"
+          checked={stateGetter(step.id)}
+          onChange={() => stateToggler(step.id)} />
         {step.label}
         {step.callout && <CallOut text={step.callout} ste={"HIDDEN"} />}
       </label>
