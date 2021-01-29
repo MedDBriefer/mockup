e3bf8259be5f7e464eb6adcb64331f2504309f9f
Scott Silliman <=>
2021-01-29 17:13:30 -0500

    changed div to ul

--- src/components/MDBDetailsPane.js
+++ src/components/MDBDetailsPane.js
@@ -5,11 +5,17 @@ import DetailsItem from "./DetailsItem"
 const MDBDetailsPane = ({children, stateGetter, stateToggler}) => {
 
     return (
-    <div>
+    <ul>
         {children.map((child) =>
-            <DetailsItem key={child.id} item={child} stateGetter={stateGetter} stateToggler={stateToggler} />
+            <li key={child.id}>
+                <DetailsItem
+                    item={child}
+                    stateGetter={stateGetter}
+                    stateToggler={stateToggler} />
+            </li>
+
         )}
-    </div>
+    </ul>
     )
 }
 
