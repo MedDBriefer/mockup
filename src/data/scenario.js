863b6abbf677c6c466480d98c717a65ec8187d22
Scott Silliman <=>
2021-01-29 17:15:00 -0500

    added more data

--- src/data/scenario.js
+++ src/data/scenario.js
@@ -8,18 +8,39 @@ export const scenario = Object.assign({}, data, {
     dispatchInfo: "You and your partners work for a transporting EMS agency and have been dispatched to a residential housing development for a lawn mower accident. It is 1330 hr on a Saturday afternoon and the temperature is 90°F (32°C). You are 5 minutes away from a level III trauma center and 25 minutes from a level I trauma center.",
     sceneAssessment: "You arrive at a suburban yard where you observe a large riding lawn mower on its side at the bottom of an 8-ft (2.5-m) hill. A bystander is holding a towel to the patient’s right lower leg. As you approach the patient, the bystander tells you that he saw the mower tip over while the patient was driving it.",
   },
-  callouts: [{
-      id: "callout-10",
-      step: "40",
-      message: "scene is safe"
-    }, {
-      id: "callout-20",
-      step: "50",
-      message: "Patient experienced a rollover with a riding lawn mower"
-    }, {
-      id: "callout-30",
-      step: "60",
-      message: "1 patient"
-    }
-  ],
+  callouts: {
+    "assess-scene-safety": "scene is safe",
+    "assess-injury-mechanism": "Patient experienced a rollover with a riding lawn mower",
+    "assess-num-patients": "1 patient",
+    "request-addl-help": "Additional EMS is on the way.",
+    "determines-loc": "Patient is unresponsive.",
+    "assess-airway": "Airway is patent",
+    "assess-breathing": "8 BPM, shallow, irregular",
+    "assess-pulse": "130; weak radial pulses",
+    "assess-mouth-nose-face": "Multiple lacerations with minor bleeding",
+    "assess-perrl": "Pupils equal, round, and responsive",
+    "assess-trachea": "Neck unremarkable",
+    "assess-jugular": "No JVD",
+    "inspects-chest": "Multiple lacerations; red marks to right chest; flail segment to right upper chest",
+  },
+  initialVitalSigns: {
+    BP: "96 / palpation",
+    P: "130, weak radial pulses",
+    R: "38, shallow; LS clear and equal with crepitus on right",
+    Skin: "Cyanonic, diaphoretic",
+    Spo2: "78 % /RA",
+    ETCO2: "64 mm Hg",
+    GCS: "8 (E-2, V-2, M-4), PERRLA",
+    Glucose: "86 mg/dl(4.8 mmol / l)",
+    Pain: "Unable to access",
+    Temp: "96.5 F(35.8 C)"
+  },
+  SAMPLE: {
+    S: "shortness of breath",
+    A: "unknown",
+    M: "unknown",
+    P: "unknown",
+    L: "unknown",
+    E: "Riding a lawnmower on an incline and it rolled over the patient"
+  }
 });
