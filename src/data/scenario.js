dc7e2a916ff32b7e07bb5927e345ad64cba23cfc
Collin O'Connor <cmo56@pitt.edu>
2021-01-30 15:33:29 -0500

    Includes callouts for appropriate items

--- src/data/scenario.js
+++ src/data/scenario.js
@@ -13,16 +13,33 @@ export const scenario = Object.assign({}, data, {
     "assess-injury-mechanism": "Patient experienced a rollover with a riding lawn mower",
     "assess-num-patients": "1 patient",
     "request-addl-help": "Additional EMS is on the way.",
-    "determines-loc": "Patient is unresponsive.",
-    //"assess-airway": "Airway is patent", // Info says airway requires management (non-patent)
-    "clears-airway": "Patient has a gag reflex in response to OPA",
-    "assess-breathing": "8 BPM, shallow, irregular", // ** point to vitals?
-    "assess-pulse": "130; weak radial pulses", // ** point to vitals?
+    "verbalizes-patient-condition": "You arrive at a suburban yard where you observe a large riding lawn mower on its side at the bottom of an 8-ft (2.5-m) hill. A bystander is holding a towel to the patient’s right lower leg. As you approach the patient, the bystander tells you that he saw the mower tip over while the patient was driving it.",
+    "determines-loc": "Opens eyes to pain and becomes progressively more unresponsive",
+    "determines-life-threats": "Multiple lacerations to face, scalp, right arm, amputation at the right ankle",
+    "assess-airway": "Airway is patent",
+    "assess-breathing": "8 BPM", // point to vitals?
+    "assess-ventilation": "shallow, irregular",
+    "assess-pulse": "130; weak radial pulses", // point to vitals?
+    "assess-skin": "Cyanonic, diaphoretic",
+    "control-bleeding": "Major external hemorrhage at lower right leg",
     "assess-mouth-nose-face": "Multiple lacerations with minor bleeding",
-    "assess-perrl": "Pupils equal, round, and responsive", // ** point to vitals?
+    "assess-scape-ears": "Unremarkable",
+    "assess-perrl": "Pupils equal, round, and responsive", // point to vitals?
     "assess-trachea": "Neck unremarkable",
     "assess-jugular": "No JVD",
-    "inspects-chest": "Multiple lacerations; red marks to right chest; flail segment to right upper chest",
+    "assess-spine": "Unremarkable / No step-off",
+    "inspects-chest": "Multiple lacerations; red marks to right chest",
+    "palpate-chest": "Flail segment to right upper chest",
+    "auscultate-chest": "Crepitus on right",
+    "assess-abdomen": "Soft, nontender",
+    "assess-pelvis": "Stable",
+    "assess-nads-taint": "Unremarkable",
+    "assess-left-leg": "Multiple lacerations",
+    "assess-right-leg": "Amputation of right leg at the ankle",
+    "assess-left-arm": "Multiple lacerations",
+    "assess-right-arm": "Multiple lacerations",
+    "assess-posterior-thorax": "Unremarkable",
+    "assess-lumbar-buttocks": "Unremarkable"
   },
   initialVitalSigns: {
     BP: "96 / palpation",
