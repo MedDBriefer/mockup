a7232bda481efd77c9d6f94bd5302a18680c47e0
Scott Silliman <=>
2021-02-01 11:08:07 -0500

    now an instance of BrowserRouter which switches to between mockups. buttons are in navbar

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react"
+
+import MDBNavBar from "./components/mdb-navbar"
+import MDBModal from "./components/MDBModal"
+
+import Mockup1 from "./Mockup1"
+import Mockup2 from "./Mockup2"
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+
+const App = (props) => {
+    const [showInfo, toggleShowInfo] = useState(false);
+    const scen = props.scenario;
+
+    return (
+        <Router>
+            <MDBNavBar
+                title={scen.info.name}
+                toggler={toggleShowInfo}
+            />
+            <Switch>
+                <Route exact path="/">
+                    <h3>Click on one of the mockups listed above</h3>
+                </Route>
+                <Route path="/mockup1">
+                    <Mockup1 scenario={props.scenario} />
+                </Route>
+                <Route path="/mockup2">
+                    <Mockup2 scenario={props.scenario} />
+                </Route>
+            </Switch>
+
+            <MDBModal
+                title="Scenario Info"
+                show={showInfo}
+                toggler={toggleShowInfo}
+            >
+                <h3>Dispatch Information</h3>
+                <p>{scen.info.dispatchInfo}</p>
+                <h3>Scene Assessment</h3>
+                <p>{scen.info.sceneAssessment}</p>
+            </MDBModal>
+
+        </Router>
+    )
+}
+
+export default App;
