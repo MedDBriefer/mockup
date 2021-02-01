b196a853eb78a9cc80dab2511a9661e513bcc16c
Scott Silliman <=>
2021-02-01 11:18:18 -0500

    changed some comments

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import { scenarioBuilder } from "./scenario-builder";
 import { scenario } from "./data/scenario";
 
-
 import App from "./App"
 
 // hack to load in the scenario data. this would be coming from some database
@@ -12,21 +11,15 @@ import App from "./App"
 // scenarioBuilder() reads in scen file which in turn loads in the trauma file and
 // *weaves* them together.  this would be done beforehand by some scenario editing
 // too, but it's instantaneous, so I'm just doing it here for now
-
 let builtScen = scenarioBuilder(scenario);
 
-
-
 // reactstrap is using some deprecated stuff, which is causing React.StrictMode
-// to output warnings which make the console difficult to read
+// to output warnings which make the console difficult to read. Disabling for now
+// <React.StrictMode>
+//     <App scenario={builtScen} />
+// </React.StrictMode>
 
-
-    // <React.StrictMode>
-    //     <App scenario={builtScen} />
-    //   </React.StrictMode>
-
-// branding = { target }
 ReactDOM.render(
-  <App  scenario={builtScen} />,
+  <App scenario={builtScen} />,
   document.getElementById("root")
 );
