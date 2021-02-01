f0681581b27087a96c9cc2d31fc88e447bc996d5
Scott Silliman <=>
2021-02-01 11:13:28 -0500

    added tabs, which are router links to allow switching between mockups

--- src/components/mdb-navbar.js
+++ src/components/mdb-navbar.js
@@ -2,23 +2,34 @@ import React from 'react';
 
 import {
     Button,
+    Nav,
+    NavItem,
+    // NavLink
     Navbar,
+    // NavbarBrand,
     NavbarText,
-    NavbarBrand,
 } from "reactstrap";
-// Nav,
-//     NavItem,
-// NavLink
+
+import MockupButton from "./MockupButton"
 
 
 
 
 const MDBNavBar = ({branding, title, toggler}) => {
     return (
-        <Navbar color="primary" dark>
-            <NavbarBrand href="#">
+        <Navbar color="dark" dark>
+            {/* <NavbarBrand href="#">
                 {branding}
             </NavbarBrand>
+             */}
+            <Nav tabs>
+                <NavItem>
+                    <MockupButton to="/mockup1" label="Mockup 1" />
+                </NavItem>
+                <NavItem>
+                    <MockupButton to="/mockup2" label="Mockup 2" />
+                </NavItem>
+            </Nav>
            {!!title &&
                 <NavbarText tag="h3">{title}</NavbarText>
             }
