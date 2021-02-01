54ea01dac4f6a30007f8bfeb4441391cf3c6d768
Scott Silliman <=>
2021-02-01 11:10:49 -0500

    removed a lot of hackish stuff

--- src/index.js
+++ src/index.js
@@ -4,24 +4,8 @@ import ReactDOM from "react-dom";
 import { scenarioBuilder } from "./scenario-builder";
 import { scenario } from "./data/scenario";
 
-// hack to allow us to create multiple mockups of app with CRA only having
-// a single index.html.  simply set in the .env file REACT_APP_BUILD_TARGET=<app_name>
-// and the value of 'target' (below) can be branched on to use different apps
-// which make use of different sets of components (or variants of them)
 
-import App1 from "./App1";
-import App2 from "./App2";
-
-let target = process.env.REACT_APP_BUILD_TARGET;
-
-let App;
-switch (target) {
-  case "app2":
-    App = App2;
-    break;
-  default:
-    App = App1;
-}
+import App from "./App"
 
 // hack to load in the scenario data. this would be coming from some database
 // after you logged in and clicked on your assignment, but this works for now
@@ -35,24 +19,14 @@ let builtScen = scenarioBuilder(scenario);
 
 // reactstrap is using some deprecated stuff, which is causing React.StrictMode
 // to output warnings which make the console difficult to read
-// let Mode;
-// switch(process.env.NODE_ENV) {
-//   case 'development':
-//     // passes the scenario data to the app as props.  passing 'target' as branding
-//     // (for the navbar) so we know which app variant we're working with
-//     Mode = () => <App branding={target} scenario={builtScen} />;
-//     break;
-//   default:
-//     Mode = () => (
-//     // ditto
-//     <React.StrictMode>
-//         <App branding={target} scenario={builtScen} />
-//       </React.StrictMode>
-//     )
-// }
-// <Mode />,
 
+
+    // <React.StrictMode>
+    //     <App scenario={builtScen} />
+    //   </React.StrictMode>
+
+// branding = { target }
 ReactDOM.render(
-  <App branding={target} scenario={builtScen} />,
+  <App  scenario={builtScen} />,
   document.getElementById("root")
 );
