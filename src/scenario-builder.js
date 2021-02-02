48bd9b4c2ea7701064ab5d6514759d56b01f2f58
Scott Silliman <=>
2021-02-02 10:48:38 -0500

    now adds callouts to items then weaves items into steps

--- src/scenario-builder.js
+++ src/scenario-builder.js
@@ -1,28 +1,41 @@
-
+// eslint-disable-next-line
 function annotateItems(items, callouts) {
-  return items;
-  // eslint-disable-next-line
-  for (let callout of callouts) {
-    // eslint-disable-next-line
-    for (const [heading, children] of Object.entries(items)) {
-      console.log(heading)
+  for (const [id, text] of Object.entries(callouts)) {
+    // console.log(id, text)
+    for (const key of Object.keys(items)) {
+      let children = items[key].map((item) =>
+        item.id === id
+        ?  {...item, callout: text}
+        : item
+      )
+      items[key] = children
     }
   }
+  return items;
+}
+
+function addEmptyChildren(array) {
+  return array.map((item) =>
+    !item.children
+    ? {...item, children: []}
+    : item
+  )
 }
 
 // eslint-disable-next-line
-function annotateStepsTree(steps, callouts) {
-  console.log(steps);
-  console.log(callouts)
+function annotateStepsTree(steps, items) {
+  // console.log(steps);
+  // console.log(items)
   return steps.map((step) => {
       if (!step.children) {
-      let callout = callouts.find((ca) => ca.step === step.id);
-      // let callout = callouts[]
-      if (callout) {
-        step.callout = callout.id;
-      }
-    } else {
-      step.children = annotateStepsTree(step.children, callouts);
+        step.children = (step.id in items) ? addEmptyChildren(items[step.id]) : []
+        // let callout = callouts.find((ca) => ca.step === step.id);
+        // // let callout = callouts[]
+        // if (callout) {
+        //   step.callout = callout.id;
+        // }
+      } else {
+        step.children = annotateStepsTree(step.children, items);
     }
     return step;
   });
@@ -33,12 +46,12 @@ export const scenarioBuilder = (scen) => {
   // console.log(info)
   // console.log(callouts)
   // console.log(outline)
-  // items
+  items = annotateItems(items, callouts)
   let newScen = {
     info,
     callouts,
-    steps: outline,
-    items: annotateItems(items, callouts),
+    steps: annotateStepsTree(outline, items),
+    items,
     criticalCriteria
   };
   return newScen;
