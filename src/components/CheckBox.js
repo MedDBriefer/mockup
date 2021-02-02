2f84b41269f49fdd318d37bd417f271b9ae415ea
Scott Silliman <=>
2021-02-02 10:57:07 -0500

    now uses List and Input instead of html tags.  passes state to Callout

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -2,17 +2,19 @@ import React from "react";
 
 import CallOut from "./CallOut";
 
+import {Input, Label } from "reactstrap"
 const CheckBox = ({ step, stateGetter, stateToggler }) => {
 
   return (
-      <label className={step.type === 'critical-criteria' ? "text-danger" : "text-default"}>
-        <input
+      <Label className={step.type === 'critical-criteria' ? "text-danger" : "text-default"}>
+        <Input
           type="checkbox"
           checked={stateGetter(step.id)}
-          onChange={() => stateToggler(step.id)} />
-        {step.label}
-        {step.callout && <CallOut text={step.callout} ste={"HIDDEN"} />}
-      </label>
+          onChange={() => stateToggler(step.id)}
+        />
+        <b>{step.label}</b>
+        {step.callout && <CallOut text={step.callout} show={stateGetter(step.id)} />}
+      </Label>
   );
 };
 
