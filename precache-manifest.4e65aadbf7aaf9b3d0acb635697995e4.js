8cb1100fd824abca1919c98afe5d640c0aef3d61
Scott Silliman <=>
2021-02-02 14:24:36 -0500

    Updates

---
+++ precache-manifest.4e65aadbf7aaf9b3d0acb635697995e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb1740c29a98adb00fe677a2790e589",
+    "url": "/mockup/index.html"
+  },
+  {
+    "revision": "6136d5d5dc462fcba050",
+    "url": "/mockup/static/css/2.032730b7.chunk.css"
+  },
+  {
+    "revision": "a4d52ab3a3b8acd67baf",
+    "url": "/mockup/static/css/main.a98b3ef1.chunk.css"
+  },
+  {
+    "revision": "6136d5d5dc462fcba050",
+    "url": "/mockup/static/js/2.4b6d2bce.chunk.js"
+  },
+  {
+    "revision": "610359362e2f9d5fa420eb531608a3c9",
+    "url": "/mockup/static/js/2.4b6d2bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d52ab3a3b8acd67baf",
+    "url": "/mockup/static/js/main.46c54ddb.chunk.js"
+  },
+  {
+    "revision": "013a9f0da0617a419927",
+    "url": "/mockup/static/js/runtime-main.3d08d490.js"
+  }
+]);
