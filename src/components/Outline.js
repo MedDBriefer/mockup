e7f72e57c50451e24068d4d0dd3b45585dadaa52
Scott Silliman <=>
2021-02-10 16:06:28 -0500

    how headings without any leaf nodes are displayed, need to style them

--- src/components/Outline.js
+++ src/components/Outline.js
@@ -2,33 +2,47 @@ import React from "react"
 
 import {Button} from "reactstrap"
 
-const Outline = ({switcher, steps, depth=3, first=false}) => {
+// I guess I don't need these functions anymore, now that they are only
+// evaluated in a single location
+function hasChildren(step) {
+    return step.children && step.children.length > 0
+}
+function allChildrenAreHeadings(step) {
+    return step.children.every((child) => child.type === 'heading')
+}
 
+
+const Outline = ({switcher, steps, depth=3, first=false}) => {
 
     return (
         <ul className={first ? "first" : ""}>
-            {steps.map((step) => (
+        {steps.map((step) => (
             <li key={step.id}>
-
-                <Button
-                    color="link"
-                    onClick={() => switcher(step.id)}>
-                    <span className={`h${depth + 1}`}>
-                        {step.label}
-                    </span>
-                </Button>
-
-                {
-                    step.children && step.children.length > 0 && step.children[0].type === 'heading' &&
-                    <Outline
-                        switcher={switcher}
-                        steps={step.children}
-                        depth={depth+1} />
-                }
+            {
+                (hasChildren(step) && allChildrenAreHeadings(step))
+                ?
+                    <>
+                        <div>
+                            {step.label}
+                        </div>
+                        <Outline
+                            switcher={switcher}
+                            steps={step.children}
+                            depth={depth+1}
+                        />
+                    </>
+                :
+                    <Button
+                        color="link"
+                        onClick={() => switcher(step.id)}>
+                        <span className={`h${depth + 1}`}>
+                            {step.label}
+                        </span>
+                    </Button>
+            }
             </li>
 
-            ))}
-
+        ))}
         </ul>
     )
 }
