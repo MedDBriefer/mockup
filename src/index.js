14ceb3017826f6a0f7e6d3f13e0ba7bac1207461
Scott Silliman <=>
2021-02-10 16:01:54 -0500

    removed ad-hoc stuff. now pretty much CRA-standard

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { scenarioBuilder } from "./scenario-builder";
-import { scenario } from "./data/scenario";
-
 import App from "./App"
 
-// hack to load in the scenario data. this would be coming from some database
-// after you logged in and clicked on your assignment, but this works for now
-// scenarioBuilder() reads in scen file which in turn loads in the trauma file and
-// *weaves* them together.  this would be done beforehand by some scenario editing
-// too, but it's instantaneous, so I'm just doing it here for now
-let builtScen = scenarioBuilder(scenario);
-// console.log(builtScen)
 // reactstrap is using some deprecated stuff, which is causing React.StrictMode
 // to output warnings which make the console difficult to read. Disabling for now
 // <React.StrictMode>
@@ -20,6 +10,6 @@ let builtScen = scenarioBuilder(scenario);
 // </React.StrictMode>
 
 ReactDOM.render(
-  <App scenario={builtScen} />,
+  <App />,
   document.getElementById("root")
 );
