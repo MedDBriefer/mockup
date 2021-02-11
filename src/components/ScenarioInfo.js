ef27974bec632cf00fe90ea5e397ec9b53ce73b4
Scott Silliman <=>
2021-02-11 08:36:23 -0500

    layed out all current info in rows/cols

--- src/components/ScenarioInfo.js
+++ src/components/ScenarioInfo.js
@@ -2,13 +2,33 @@
 import React from "react"
 
 export default function ScenarioInfo({scenario}) {
-
+    const info = scenario.info;
     return (
-        <>
-            <h3>Dispatch Information</h3>
-            <p>{scenario.info.dispatchInfo}</p>
-            <h3>Scene Assessment</h3>
-            <p>{scenario.info.sceneAssessment}</p>
-        </>
-    )
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-6">
+                    <h3>Instructor Information</h3>
+                    <p>{info.instructorInformation}</p>
+                </div>
+                <div className="col-6">
+                    <h3>Patient Information</h3>
+                    <ul>
+                        <li><b>Moulage:</b> {info.patientInformation.moulage}</li>
+                        <li><b>Position:</b> {info.patientInformation.position}</li>
+                        <li><b>Actions:</b> {info.patientInformation.actions}</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6">
+                    <h3>Dispatch Information</h3>
+                    <p>{info.dispatchInfo}</p>
+                </div>
+                <div className="col-6">
+                    <h3>Scene Assessment</h3>
+                    <p>{info.sceneAssessment}</p>
+                </div>
+            </div>
+        </div>
+)
 }
