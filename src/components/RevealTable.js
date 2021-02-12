cbebf515c6c1302451c2e3afda83eb930762b922
Scott Silliman <=>
2021-02-12 13:23:50 -0500

    made use of new Panel component

--- src/components/RevealTable.js
+++ src/components/RevealTable.js
@@ -1,45 +1,40 @@
 import React from "react"
 
 import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardBody,
+    // Card,
+    // CardHeader,
+    // CardTitle,
+    // CardBody,
     Table
 } from "reactstrap"
 
 // eslint-disable-next-line no-unused-vars
 import classnames from "classnames"
 
+import Panel from "./Panel"
 import RevealTableRow from "./RevealTableRow"
 
 export default function RevealTable({heading, rows=[]}) {
 
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle className="h3 text-center">{heading}</CardTitle>
-            </CardHeader>
-            <CardBody>
-                <Table striped={true}>
-                    <thead>
-                        <tr>
-                            <th>Type</th>
-                            <th>Value</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {rows.map((row, index) =>
-                            <RevealTableRow
-                                key={index}
-                                label={row.label}
-                                value={row.value}
-                            />
-                        )}
-                    </tbody>
-                </Table>
-            </CardBody>
-        </Card>
-
+        <Panel title={heading} titleSize={"h5"}>
+            <Table striped={true}>
+                <thead>
+                    <tr>
+                        <th>Type</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows.map((row, index) =>
+                        <RevealTableRow
+                            key={index}
+                            label={row.label}
+                            value={row.value}
+                        />
+                    )}
+                </tbody>
+            </Table>
+        </Panel>
     )
 }
