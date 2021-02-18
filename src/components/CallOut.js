ac87f70c4c97699ee39f5376cebe8defde11a7d0
Scott Silliman <=>
2021-02-18 09:10:55 -0500

    switched to material icons

--- src/components/CallOut.js
+++ src/components/CallOut.js
@@ -5,7 +5,8 @@ const CallOut = ({ text, show }) => {
   if (show) {
     return <span className="callout">{text}</span>;
   } else {
-      return <i className="callout-icon fa fa-comment" />;
+      // return <i className="callout-icon fa fa-comment" />;
+      return <span class="material-icons">message</span>
   }
 }
 export default CallOut;
