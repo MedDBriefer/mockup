058f4ac94fd85d946794760e67b625609374c4c8
Scott Silliman <=>
2021-02-18 14:08:02 -0500

    partial refactoring. no longer uses <input> and uses getter/setter directly

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -3,37 +3,62 @@ import React from "react";
 import CallOut from "./CallOut";
 
 import {Input, Label } from "reactstrap"
-// import classnames from "classnames"
+import classnames from "classnames"
+
 
 const CheckBox = ({ step, config}) => {
 
-  // pseudo-code for new implementation
-  // const NewCheckbox = (
-  //   <span>
+// const CheckBox = ({ step, inlineChildren=[], blockChildren=[], config }) => {
+
+  // pseudo-code for new implementation (add optional inline & block children props???)
+  // return (
+  //   <label>
   //     <span className="material-icons">
   //       {isChecked(step.id) ? "check_box": "check_box_outline_blank"}
   //     </span>
   //     <b>{step.label}</b>
-  //   </span>
+  //     {...inlineChildren}  // things like callout-icon, intervention-form-button // things you want on same line
+  //   </label>
+  //   { !!blockChildren
+  //     <div>
+  //      {...blockChildren}  //things like intervention form (children you want to
+  //     </div>               // like like they're on the next line - after a <br> or sometthing )
+  //   }
   // )
 
   return (
-      <Label className={step.type === 'critical-criteria' ? "text-danger" : "text-default"}>
-        <Input
-          type="checkbox"
-          checked={config.isChecked(step.id)}
-          onChange={() => config.toggleChecked(step.id)}
+    <label className={classnames({"text-danger": step.type === 'critical-criteria'})}
+      onClick={() => config.toggleChecked(step.id)}>
+      <span className="checkbox-icon material-icons-outlined">
+        {config.isChecked(step.id) ? "check_box": "check_box_outline_blank"}
+      </span>
+      <b>{step.label}</b>
+      { !!step.callout &&
+        <CallOut
+          step={step}
+          config={config}
         />
-        <b>{step.label}</b>
+      }
+    </label>
+  )
 
-        { !!step.callout &&
-          <CallOut
-            step={step}
-            config={config}
-          />
-        }
-      </Label>
-  );
-};
+  // return (
+  //     <Label className={step.type === 'critical-criteria' ? "text-danger" : "text-default"}>
+  //       <Input
+  //         type="checkbox"
+  //         checked={config.isChecked(step.id)}
+  //         onChange={() => config.toggleChecked(step.id)}
+  //       />
+  //       <b>{step.label}</b>
+  //       { !!step.callout &&
+  //         <CallOut
+  //           step={step}
+  //           config={config}
+  //         />
+  //       }
+  //     </Label>
+  // );
+
+}
 
 export default CheckBox;
