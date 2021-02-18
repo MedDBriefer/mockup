c9f2da4d728fcab43f147b4b0a804a54b5fac826
Scott Silliman <=>
2021-02-18 09:10:04 -0500

    added (commented out)  pseudo-code suggesting refactoring

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -3,7 +3,19 @@ import React from "react";
 import CallOut from "./CallOut";
 
 import {Input, Label } from "reactstrap"
+// import classnames from "classnames"
+
 const CheckBox = ({ step, isChecked, toggleChecked , showCallouts, showCalloutIcon}) => {
+
+  // pseudo-code for new implementation
+  // const NewCheckbox = (
+  //   <span>
+  //     <span className="material-icons">
+  //       {isChecked(step.id) ? "check_box": "check_box_outline_blank"}
+  //     </span>
+  //     <b>{step.label}</b>
+  //   </span>
+  // )
 
   return (
       <Label className={step.type === 'critical-criteria' ? "text-danger" : "text-default"}>
