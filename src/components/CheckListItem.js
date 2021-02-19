f0c98cdf259915c5792ce609969ccd522cd7b35c
Scott Silliman <=>
2021-02-19 18:33:56 -0500

    refactored so that it imports all components and perform business logic regarding what components to pass to CheckBox

--- src/components/CheckListItem.js
+++ src/components/CheckListItem.js
@@ -1,20 +1,66 @@
 import React from "react"
 
 import CheckBox from "./CheckBox"
+import CallOut from "./CallOut"
 
-const CheckListItem = ({scenario, item, config}) => {
+const CheckListItem = ({scenario, step, config}) => {
 
-    switch (item.type) {
-        case "assessment":
-        case "critical-criteria":
-        case "required-action":
-            return <CheckBox
-                        step={item}
-                        config={config}
-                    />
+    switch (step.type) {
+        case 'assessment':
+            // replace this with both CallOutIcon and CallOutText (or whatever you name them)
+            const inlines = (<CallOut scenario={scenario} step={step} config={config} />)
+            return (
+                <CheckBox
+                    scenario={scenario}
+                    step={step}
+                    config={config}
+                    clickHandler={config.toggleChecked}
+                    inlineChildren={inlines}
+                />
+            )
+        case 'required-action':
+        case 'critical-criteria':
+        case 'execution-error':
+            return (
+                <CheckBox
+                    scenario={scenario}
+                    step={step}
+                    clickHandler={config.toggleChecked}
+                    config={config}
+                />
+            )
+        // case 'intervention':
+            // const inlines = (
+            //     <InterventionButton
+            //         scenario={scenario}
+            //         step={step}
+            //         config={config}
+            //     />
+            // )
+            //
+            // pass some component which simply wraps an InterventionForm with a div and determines
+            // whether to display itself based by setting .hidden if getDispForm(step.id) or something like that.
+            // return (
+            //     <CheckBox
+            //         scenario={scenario}
+            //         step={step}
+            //         config={config}
+            //         clickHandler={config.toggleForm}
+            //         inlineChildren={inlines}
+            //     >
+            //         <WrappedInterventionForm
+            //             scenario={scenario}
+            //             step={step}
+            //             config={config}
+            //         />
+            //     </CheckBox>
+            // )
         default:
-            return <p>{JSON.stringify(item, null, 4)}</p>
-    }
+            return <p>{JSON.stringify(step, null, 4)}</p>
+        }
+
 }
+
+
 
 export default CheckListItem;
