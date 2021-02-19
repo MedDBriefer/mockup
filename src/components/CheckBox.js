90bd7cda23159a02e21a95e0dc44fe2b6592e295
Scott Silliman <=>
2021-02-19 18:32:30 -0500

    improved readability

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,19 +1,23 @@
 import React from "react";
 
-// import CallOut from "./CallOut";
 
 import classnames from "classnames"
 
 
 const CheckBox = (props) => {
   // need props param to exist in order to access props.children
+  // eslint-disable-next-line no-unused-vars
   const { scenario, step, clickHandler, inlineChildren = [], config } = props
 
   return (
     <>
-      <label className={classnames({"text-danger": step.type === 'critical-criteria'})}
-        onClick={() => clickHandler(step.id)}>
-        <span className="checkbox-icon material-icons-outlined">
+      <label
+        className={classnames({"text-danger": step.type === 'critical-criteria'})}
+        onClick={() => clickHandler(step.id)}
+      >
+        <span
+          className="checkbox-icon material-icons-outlined"
+        >
           {config.isChecked(step.id) ? "check_box": "check_box_outline_blank"}
         </span>
         <b>{step.label}</b>
