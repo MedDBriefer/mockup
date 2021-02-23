19f43891a5acda45c6320d116dd095326de68c06
Scott Silliman <=>
2021-02-23 18:58:52 -0500

    re-implemented as class component added PreferencesContext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, {useState} from "react"
+import React, {
+    // useState
+} from "react"
 
 import MDBNavBar from "./components/MDBNavBar"
 import MDBModal from "./components/MDBModal"
 import ScenarioInfo from "./components/ScenarioInfo"
 import PreferencesForm from "./components/PreferencesForm"
+import {defaultPrefs, PreferencesContext} from "./components/PreferencesContext"
 
 import Mockup1 from "./Mockup1"
 import Mockup2 from "./Mockup2"
@@ -21,59 +24,114 @@ import "./styles.css";
 import scenarioData  from "./data/scenario"
 import traumaScenarioIndexer from "./data/traumaScenarioIndexer"
 
-const scen = traumaScenarioIndexer(scenarioData)
-console.log(scen)
 
-const App = (props) => {
 
-    const [showInfo, setShowInfo] = useState(false);
-    const toggleShowInfo = () => {
-        setShowInfo(!showInfo);
+class App extends React.Component {
+
+    constructor(props) {
+        console.log("App constructor called")
+        super(props)
+        this.state = {
+            showInfo: false,
+            showPrefs: false,
+            ...defaultPrefs
+        }
+        // dispCheckBoxesInline: false,
+        //     dispCallOutsInline: false,
+        //         dispFormsInline: false,
+        //             hideRaterInfo: false
+
+        this.scen = traumaScenarioIndexer(scenarioData)
+        console.log(this.scen)
+
+        this.toggleShowInfo = this.toggleShowInfo.bind(this)
+        this.toggleShowPrefs = this.toggleShowPrefs.bind(this)
+        this.getPref = this.getPref.bind(this)
+        this.setPref = this.setPref.bind(this)
+        this.saveState = this.saveState.bind(this)
     }
-    const [showPrefs, setShowPrefs] = useState(false);
-    const toggleShowPrefs = () => {
-        setShowPrefs(!showPrefs);
+
+    toggleShowInfo() {
+        this.setState((prevState) => ({
+            showInfo: !prevState.showInfo
+        }))
+    }
+    toggleShowPrefs() {
+        this.setState((prevState) => ({
+            showPrefs: !prevState.showPrefs
+        }))
     }
 
-    return (
-        <Router>
-            <MDBNavBar
-                title={scen.id}
-                scenInfoToggler={toggleShowInfo}
-                prefsFormToggler={toggleShowPrefs}
-            />
+    getPref(pref) {
+        return this.state[pref]
+    }
 
-            <Switch>
-                <Route path="/mockup1">
-                    <Mockup1 scenario={scen} />
-                </Route>
-                <Route path="/mockup2">
-                    <Mockup2 scenario={scen} />
-                </Route>
-                <Route path="/mockup3">
-                    <Mockup3 scenario={scen} />
-                </Route>
-                <Route path="*">
-                    <h3>Click on one of the mockups listed above</h3>
-                </Route>
-            </Switch>
+    setPref(pref, value) {
+        this.setState({
+            [pref]: value
+        }, () => this.saveState())
+    }
 
-            <MDBModal
-                title="Scenario Info"
-                show={showInfo}
-                toggler={toggleShowInfo}
-            >
-                <ScenarioInfo scenario={scen} />
-            </MDBModal>
-            <MDBModal
-                title="User Preferences"
-                show={showPrefs}
-                toggler={toggleShowPrefs}
-            >
-                <PreferencesForm />
-            </MDBModal>
-        </Router>
-    )
+
+    saveState() {
+        // simply dump to console for now. will eventually persist to db
+        console.log(this.state)
+    }
+
+    // const [showInfo, setShowInfo] = useState(false);
+    // const toggleShowInfo = () => {
+    //     setShowInfo(!showInfo);
+    // }
+    // const [showPrefs, setShowPrefs] = useState(false);
+    // const
+
+
+    render () {
+
+        return (
+            <Router>
+
+                <MDBNavBar
+                    title={this.scen.id}
+                    scenInfoToggler={this.toggleShowInfo}
+                    prefsFormToggler={this.toggleShowPrefs}
+                />
+
+                <Switch>
+                    <Route path="/mockup1">
+                        <Mockup1 scenario={this.scen} />
+                    </Route>
+                    <Route path="/mockup2">
+                        <Mockup2 scenario={this.scen} />
+                    </Route>
+                    <Route path="/mockup3">
+                        <Mockup3 scenario={this.scen} />
+                    </Route>
+                    <Route path="*">
+                        <h3>Click on one of the mockups listed above</h3>
+                    </Route>
+                </Switch>
+
+                <MDBModal
+                    title="Scenario Info"
+                    show={this.state.showInfo}
+                    toggler={this.toggleShowInfo}
+                >
+                    <ScenarioInfo scenario={this.scen} />
+                </MDBModal>
+                    <MDBModal
+                        title="User Preferences"
+                        show={this.state.showPrefs}
+                        toggler={this.toggleShowPrefs}
+                    >
+                        <PreferencesContext.Provider value={{ getPref: this.getPref, setPref: this.setPref }}>
+                            <PreferencesForm hideModal={this.toggleShowPrefs} />
+                        </PreferencesContext.Provider>
+                    </MDBModal>
+            </Router>
+        )
+
+    }
 }
 
 export default App;
