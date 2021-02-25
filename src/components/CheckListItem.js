04f881569f8033d416064ac4ecc47a0229ef29ae
Scott Silliman <=>
2021-02-25 01:51:11 -0500

    removed CallOutText from inlineChildren and added as a normal child to CheckBox, and added missing test for isChecked()

--- src/components/CheckListItem.js
+++ src/components/CheckListItem.js
@@ -8,70 +8,13 @@ const CheckListItem = ({scenario, step, config}) => {
 
     switch (step.type) {
         case 'assessment':
-            // while I see you got this working, you may see the warnings reported by the "npm watch" script.
-            // I fixed the curly braces, etc and wrapped the entire thing (as it might contain more than 1 componet) with a
-            // react fragment, <> </>  (simply acts like a div to wrap multiple components) which I believe is the same as
-            // <React.Fragment> </React.Fragment>
-            //  (see the react docs, under Fragment - could be hidden under Advanced if you don't see elsewhere)
-            //
-            // const inlines = (
-            //     <>
-            //         { config.showOnlyIcon &&
-            //             <CallOutIcon
-            //                 scenario={scenario}
-            //                 step={step}
-            //                 config={config}
-            //             />
-            //         }
-            //         { !config.showOnlyIcon &&
-            //             <CallOut
-            //                 scenario={scenario}
-            //                 step={step}
-            //                 config={config}
-            //             />
-            //         }
-            //     </>
-            // )
-
-            // replace this:
             const inlines = (
-                     <>
-                            <CallOutIcon
-                                scenario={scenario}
-                                step={step}
-                                config={config}
-                            />
-                         { config.displayCalloutText &&
-                             <CallOutText
-                                 scenario={scenario}
-                                 step={step}
-                                 config={config}
-                             />
-                         }
-                     </>
-                 )
-            // with both CallOutIcon and CallOutText
-            // make CallOutText conditional based on config.dispCalloutText
-            // something like::
-            // const inlines = (
-            //     <>
-            //         <CallOutIcon scenario={scenario} step={step} config={config} />
-            //         { config.dispCallOutText &&
-            //             <CallOutText scenario={scenario} step={step} config={config} />
-            //         }
-            //     </>
-            // )
-            // or, if you'd rather have CalloutText appear *below* the checkbox line
-            // do a
-            // <CheckBox props>
-            //     {config.dispCalloutText && <CallOutText props> }
-            // </Checkbox>
-
-
-            /*in CheckListItem, for assessment steps, 
-            you'd pass toggleCallout as the clickHandler prop to CheckBox rather than toggleChecked, 
-            so you get both behaviors instead of simply the checking of the checkbox*/
-
+                <CallOutIcon
+                    scenario={scenario}
+                    step={step}
+                    config={config}
+                />
+            )
             return (
                 <CheckBox
                     scenario={scenario}
@@ -79,7 +22,19 @@ const CheckListItem = ({scenario, step, config}) => {
                     config={config}
                     clickHandler={config.toggleCallout}
                     inlineChildren={inlines}
-                />
+                >
+                    {config.displayCalloutText &&
+                        <div>
+                            {config.isChecked(step.id) &&
+                                <CallOutText
+                                    scenario={scenario}
+                                    step={step}
+                                    config={config}
+                                />
+                            }
+                        </div>
+                    }
+                </CheckBox>
             )
         case 'required-action':
         case 'critical-criteria':
@@ -89,7 +44,7 @@ const CheckListItem = ({scenario, step, config}) => {
                 <CheckBox
                     scenario={scenario}
                     step={step}
-                    clickHandler={config.toggleChecked}   
+                    clickHandler={config.toggleChecked}
                     config={config}
                 />
             )
