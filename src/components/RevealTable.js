aa440f5dfa9e9bd9a9bbbd959db6410552e91aae
Scott Silliman <=>
2021-03-04 13:15:45 -0500

    no longer wraps self in Panel

--- src/components/RevealTable.js
+++ src/components/RevealTable.js
@@ -1,38 +1,31 @@
 import React from "react"
 
-import {
-    Table
-} from "reactstrap"
+import { Table } from "reactstrap"
 
-
-import Panel from "./Panel"
 import RevealTableRow from "./RevealTableRow"
 
-export default function RevealTable({title, headings=[], rows=[], config}) {
+export default function RevealTable({title, headings=[], rows=[]}) {
 
     return (
-        <Panel title={title} titleSize={"h5"}>
-            <Table striped={true} size="sm">
-                <thead>
-                    <tr>
-                        {
-                            headings.map((heading, index) => (
-                                <th key={index}>{heading}</th>
-                            ))
-                        }
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows.map((row, index) =>
-                        <RevealTableRow
-                            key={index}
-                            label={row.label}
-                            values={row.value}
-                            config={config}
-                        />
-                    )}
-                </tbody>
-            </Table>
-        </Panel>
+        <Table striped={true} size="sm">
+            <thead>
+                <tr>
+                    {
+                        headings.map((heading, index) => (
+                            <th key={index}>{heading}</th>
+                        ))
+                    }
+                </tr>
+            </thead>
+            <tbody>
+                {rows.map((row, index) =>
+                    <RevealTableRow
+                        key={index}
+                        label={row.label}
+                        values={row.value}
+                    />
+                )}
+            </tbody>
+        </Table>
     )
 }
