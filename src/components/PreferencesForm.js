a23c8b26a5e1577d7950e5e3113e2b4772b78f8e
Scott Silliman <=>
2021-03-04 13:46:25 -0500

    now self-wraps in modal

--- src/components/PreferencesForm.js
+++ src/components/PreferencesForm.js
@@ -1,80 +1,77 @@
-import React, {
-    useContext
-} from "react"
+import React from "react"
 
-import {
-    Button,
-} from "reactstrap"
+import { Button } from "reactstrap"
 
-import {PreferencesContext} from "./PreferencesContext"
+import { usePrefs } from "../contexts/PreferencesContext"
 
-const PreferencesForm = (props) => {
+import MDBModal from "./MDBModal"
 
-    const {getPref, setPref} = useContext(PreferencesContext)
+const PreferencesForm = ({show, dismiss}) => {
+
+    const {
+        dispLeafNodesInline,
+        dispAssessmentFindingsInline,
+        inhibitRaterOversharing,
+        setPref
+    } = usePrefs()
 
     const handleInputChange = (event) => {
         const target = event.target
         const value = target.type === "checkbox" ? target.checked : target.value
         const name = target.name
         setPref(name, value)
-
     }
 
     return (
-        <div className="container">
-            <div className="row">
-                <label>
-                    <input
-                        type="checkbox"
-                        name="dispCheckBoxesInline"
-                        checked={getPref("dispCheckBoxesInline")}
-                        onChange={handleInputChange}
-                    />
-                    &nbsp; Embed Checkboxes within Checklist Outline
-                </label>
-            </div>
-            <div className="row">
-                <label>
-                    <input
-                        type="checkbox"
-                        name="dispCallOutsInline"
-                        checked={getPref("dispCallOutsInline")}
-                        onChange={handleInputChange}
-                    />
-                    &nbsp; Embed Assessment Findings within checklist
-                </label>
-            </div>
-            <div className="row">
-                <label>
-                    <input
-                        type="checkbox"
-                        name="dispFormsInline"
-                        checked={getPref("dispFormsInline")}
-                        onChange={handleInputChange}
-                    />
-                    &nbsp; Embed Intervention Forms within checklist
-                </label>
-            </div>
-            <div className="row">
-                <label>
-                    <input
-                        type="checkbox"
-                        name="hideRaterInfo"
-                        checked={getPref("hideRaterInfo")}
-                        onChange={handleInputChange}
-                    />
-                    &nbsp; Inhibit Oversharing of Information
-                </label>
-            </div>
+        <MDBModal
+            title="User Preferences"
+            show={show}
+            dismiss={dismiss}
+        >
+            <div className="container">
+                <div className="row">
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="dispLeafNodesInline"
+                            checked={dispLeafNodesInline}
+                            onChange={handleInputChange}
+                        />
+                        &nbsp; Embed Checkboxes within Checklist Outline
+                    </label>
+                </div>
+                <div className="row">
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="dispAssessmentFindingsInline"
+                            checked={dispAssessmentFindingsInline}
+                            onChange={handleInputChange}
+                        />
+                        &nbsp; Embed Assessment Findings within Checklist
+                    </label>
+                </div>
+                <div className="row">
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="inhibitRaterOversharing"
+                            checked={inhibitRaterOversharing}
+                            onChange={handleInputChange}
+                        />
+                        &nbsp; Inhibit Rater Oversharing of Information
+                    </label>
+                </div>
 
-            <Button
-                type="button"
-                className="btn btn-success"
-                onClick={() => props.hideModal()}
-            >
-                Dismiss
-            </Button>
-        </div>
+                <Button
+                    type="button"
+                    className="btn btn-success"
+                    onClick={dismiss}
+                >
+                    Dismiss
+                </Button>
+            </div>
+        </MDBModal>
     )
 }
 
