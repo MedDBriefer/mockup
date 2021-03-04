13ccbceb64d4e507c02617f677998c0b91292c4a
Scott Silliman <=>
2021-03-04 17:21:54 -0500

    disabled the ability of the Assessment Findings tab to be enabled (and not displayed) if prefs state that assessment findings are supposed to be displayed  inline

--- src/components/RaterInfo.js
+++ src/components/RaterInfo.js
@@ -1,5 +1,5 @@
 
-import React,{ useState, useContext } from "react"
+import React,{ useState, useContext, useEffect } from "react"
 
 import {
     Nav,
@@ -15,26 +15,30 @@ import { usePrefs } from "../contexts/PreferencesContext"
 import { ScenarioContext } from "../contexts/ScenarioContext"
 
 import CallOutText from "./CallOutText"
-
-
-
 import RevealTable from "./RevealTable"
 
 export default function RaterInfo({defaultTab = "callouts"}) {
     const {dispAssessmentFindingsInline} = usePrefs()
     const {scenario, currentVitals, vitalsRecomputed} = useContext(ScenarioContext)
     const [activeTab, setActiveTab] = useState(defaultTab)
-    const toggle = (tab) => {
+    const activateTab = (tab) => {
         if (activeTab !== tab) setActiveTab(tab)
     }
+    const isActiveTab = (tab) => tab === activeTab
 
-    // conditionally compute callouts array only if they are supposed to be
-    // displayed within this pane
-    let callouts = []
-    if (!dispAssessmentFindingsInline) {
-        // here you'd be passing actual Callout/CalloutText (whatever you call it)
-        // components as the value instead of merely step.callout
-        callouts = scenario.assessmentSteps.map(step => {
+    useEffect(
+        () => {
+            console.log(activeTab)
+            // disable the callouts tab ability to be active if assessment findings
+            // are supposed to be displayed inline
+            if (activeTab === "callouts" && dispAssessmentFindingsInline) {
+                setActiveTab("vitals")
+            }
+        }, [activeTab]
+    )
+
+    const getAssessmentFindingsTabPane = () => {
+        const callouts = scenario.assessmentSteps.map(step => {
             return {
                 id: step.id,
                 label: step.calloutLabel,
@@ -46,30 +50,63 @@ export default function RaterInfo({defaultTab = "callouts"}) {
                 ]
             }
         })
+        return (
+            <TabPane tabId = "callouts" >
+                <RevealTable
+                    title="Assessment Findings"
+                    headings={["Asssesment", "Finding"]}
+                    rows={callouts}
+                />
+            </TabPane >
+        )
     }
 
+    const getSampleTabPane = () => {
+        const sample = Object.entries(scenario.SAMPLE).map(rec => {
+            return { label: rec[0], value: [rec[1]] }
+        })
+        return (
+            <TabPane tabId = "sample" >
+                <RevealTable
+                    title="SAMPLE"
+                    headings={["Type", "Value"]}
+                    rows={sample}
+                />
+            </TabPane >
+        )
+    }
 
-    const sample = Object.entries(scenario.SAMPLE).map(rec => {
-        return {label: rec[0], value: [rec[1]]}
-    })
-
-
-    const vitalsHeaders =
-        vitalsRecomputed
-        ? ["Vital Sign", "Initial Value", "Current Value"]
-        : ["Vital Sign", "Current Value"]
-
-    const vitals = Object.entries(scenario.initialVitalSigns).map(iv => {
-        const value =
+    const getVitalsTabPane = () => {
+        // vitals tab will display either 2 or 3 columns, depending on
+        // whether vitals have been recomputed, and thus we need to display
+        // both the initial and current value
+        const vitalsHeaders =
             vitalsRecomputed
-            ? [ iv[1], currentVitals[iv[0]] ]
-            : [ iv[1] ]
+            ? ["Vital Sign", "Initial Value", "Current Value"]
+            : ["Vital Sign", "Current Value"]
 
-        return {
-            label: iv[0],
-            value: value
-        }
-    })
+        const vitals = Object.entries(scenario.initialVitalSigns).map(iv => {
+            const value =
+                vitalsRecomputed
+                ? [ iv[1], currentVitals[iv[0]] ]
+                : [ iv[1] ]
+
+            return {
+                label: iv[0],
+                value: value
+            }
+        })
+
+        return (
+            <TabPane tabId = "vitals" >
+                <RevealTable
+                    title="Vitals"
+                    headings={vitalsHeaders}
+                    rows={vitals}
+                />
+            </TabPane >
+        )
+    }
 
     return (
         <>
@@ -77,8 +114,8 @@ export default function RaterInfo({defaultTab = "callouts"}) {
                 {!dispAssessmentFindingsInline &&
                     <NavItem>
                         <NavLink
-                            className={classnames({ active: activeTab === 'callouts' })}
-                            onClick={() => toggle('callouts')}
+                            className={classnames({ active: isActiveTab('callouts') })}
+                            onClick={() => activateTab('callouts')}
                         >
                             Assessment Findings
                         </NavLink>
@@ -86,45 +123,25 @@ export default function RaterInfo({defaultTab = "callouts"}) {
                 }
                 <NavItem>
                     <NavLink
-                        className={classnames({ active: activeTab === 'vitals' })}
-                        onClick={() => toggle('vitals')}
+                        className={classnames({ active: isActiveTab('vitals') })}
+                        onClick={() => activateTab('vitals')}
                     >
                         Vitals
                     </NavLink>
                 </NavItem>
                 <NavItem>
                     <NavLink
-                        className={classnames({ active: activeTab === 'sample' })}
-                        onClick={() => toggle('sample')}
+                        className={classnames({ active: isActiveTab('sample') })}
+                        onClick={() => activateTab('sample')}
                     >
                         SAMPLE
                     </NavLink>
                 </NavItem>
             </Nav>
             <TabContent activeTab={activeTab}>
-                {!dispAssessmentFindingsInline &&
-                    <TabPane tabId="callouts">
-                        <RevealTable
-                            title="Assessment Findings"
-                            headings={["Asssesment", "Finding"]}
-                            rows={callouts}
-                        />
-                    </TabPane>
-                }
-                <TabPane tabId="vitals">
-                    <RevealTable
-                        title="Vitals"
-                        headings={vitalsHeaders}
-                        rows={vitals}
-                    />
-                </TabPane>
-                <TabPane tabId="sample">
-                    <RevealTable
-                        title="SAMPLE"
-                        headings={["Type", "Value"]}
-                        rows={sample}
-                    />
-                </TabPane>
+                {!dispAssessmentFindingsInline && getAssessmentFindingsTabPane() }
+                { getVitalsTabPane() }
+                { getSampleTabPane() }
             </TabContent>
         </>
     );
