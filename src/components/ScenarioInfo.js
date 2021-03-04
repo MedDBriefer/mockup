8c6546e13e5e60aff23fd31b29474b857778cc2c
Scott Silliman <=>
2021-03-04 13:11:28 -0500

    now self wraps contents in modal and recieves scenario via context

--- src/components/ScenarioInfo.js
+++ src/components/ScenarioInfo.js
@@ -1,34 +1,48 @@
 
-import React from "react"
+import React, { useContext } from "react"
 
-export default function ScenarioInfo({scenario}) {
-    const info = scenario.info;
+import MDBModal from "./MDBModal"
+
+import { ScenarioContext } from "../contexts/ScenarioContext"
+
+export default function ScenarioInfo({show, dismiss}) {
+
+    const { scenario } = useContext(ScenarioContext)
+
+
     return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-6">
-                    <h3>Instructor Information</h3>
-                    <p>{info.instructorInformation}</p>
+        <MDBModal
+            name="Scenario Info"
+            show={show}
+            dismiss={dismiss}
+        >
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-6">
+                        <h3>Instructor Information</h3>
+                        <p>{scenario.info.instructorInformation}</p>
+                    </div>
+                    <div className="col-6">
+                        <h3>Patient Information</h3>
+                        <ul>
+                            <li><b>Moulage:</b> {scenario.info.patientInformation.moulage}</li>
+                            <li><b>Position:</b> {scenario.info.patientInformation.position}</li>
+                            <li><b>Actions:</b> {scenario.info.patientInformation.actions}</li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="col-6">
-                    <h3>Patient Information</h3>
-                    <ul>
-                        <li><b>Moulage:</b> {info.patientInformation.moulage}</li>
-                        <li><b>Position:</b> {info.patientInformation.position}</li>
-                        <li><b>Actions:</b> {info.patientInformation.actions}</li>
-                    </ul>
+                <div className="row">
+                    <div className="col-6">
+                        <h3>Dispatch Information</h3>
+                        <p>{scenario.info.dispatchInfo}</p>
+                    </div>
+                    <div className="col-6">
+                        <h3>Scene Assessment</h3>
+                        <p>{scenario.info.sceneAssessment}</p>
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-6">
-                    <h3>Dispatch Information</h3>
-                    <p>{info.dispatchInfo}</p>
-                </div>
-                <div className="col-6">
-                    <h3>Scene Assessment</h3>
-                    <p>{info.sceneAssessment}</p>
-                </div>
-            </div>
-        </div>
-)
+        </MDBModal>
+
+    )
 }
