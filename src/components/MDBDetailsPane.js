b2f87277a66cea9b2cb48f18e76bd8b977c08137
Scott Silliman <=>
2021-03-12 13:40:35 -0500

    replaced Panel with a simple h3 tag

--- src/components/MDBDetailsPane.js
+++ src/components/MDBDetailsPane.js
@@ -3,7 +3,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ScenarioContext } from "../contexts/ScenarioContext"
 
 import CheckListItem from "./CheckListItem"
-import Panel from "./Panel"
+// import Panel from "./Panel"
 import RaterInfo from "./RaterInfo"
 
 
@@ -26,19 +26,26 @@ const MDBDetailsPane = () => {
     )
 
     if (null === label) {
-        return <div></div>
+        return (
+            <div style={{verticalAlign: "center"}}>
+                <p className="text-center">
+                    Please click on an outline step to the left to see it's details
+                </p>
+            </div>
+        )
     }
     return (
         <>
-            <Panel title={`${label} Details`}>
-                <ul>
-                    {children.map((child) =>
-                        <li key={child.id}>
-                            <CheckListItem step={child} />
-                        </li>
-                    )}
-                </ul>
-            </Panel>
+            <h3 className="text-center">
+                {`${label} Details`}
+            </h3>
+            <ul className="first">
+                {children.map((child) =>
+                    <li key={child.id}>
+                        <CheckListItem step={child} />
+                    </li>
+                )}
+            </ul>
             <RaterInfo defaultTab={"vitals"} />
         </>
     )
